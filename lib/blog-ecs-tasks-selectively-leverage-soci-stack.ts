02aff2fb834c485874ba1e3275de5a0a53d49968
Shinnosuke Takakuni <takakuni.shinnosuke@classmethod.jp>
2024-02-05 15:43:46 +0900

    [mod] ECS Exec を使うため、readonlyRootFilesystem: false に変更

--- lib/blog-ecs-tasks-selectively-leverage-soci-stack.ts
+++ lib/blog-ecs-tasks-selectively-leverage-soci-stack.ts
@@ -170,7 +170,6 @@ export class BlogEcsTasksSelectivelyLeverageSociStack extends cdk.Stack {
         streamPrefix: 'firelens',
         logGroup: firelensLogGroup
       }),
-      readonlyRootFilesystem: true,
       linuxParameters: new ecs.LinuxParameters(this, 'firelensParameters', {
         initProcessEnabled: true,
       }),
